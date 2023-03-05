59581adfdf7d7806f393acd46a8463568f0e8469
MateoPissarello <mateopissa@gmail.com>
2023-03-05 08:47:25 -0500

    update

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from fastapi import FastAPI
+import uvicorn
 from web_scraping_classcentral import get_data
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 @app.get("/class_central")
-def class_central():
+async def class_central():
     data = get_data()
     return data
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port = 8000)
+    uvicorn.run("main:app", port=8000, reload=True)
+
